b1d9cc86cfaa37a12e6fbca4a235f7ff8740bda5
CHEN Xian-an <xianan.chen@gmail.com>
2019-10-15 20:50:08 +0800

    Format and fix some tests on #1

--- Sources/XPaver/Doc.swift
+++ Sources/XPaver/Doc.swift
@@ -13,27 +13,25 @@ public final class Doc: Equatable {
     case xml
     case html
   }
-
+  
   public enum Error: Swift.Error {
     case invalidSourceData
     case invalidEncoding
     case noXMLRoot
   }
-
+  
   public let data: Data
   public let kind: Kind
   public let encoding: String.Encoding
-
+  
   public init(data: Data, kind: Kind, encoding: String.Encoding = .utf8) throws {
     self.data = data
     self.kind = kind
     self.encoding = encoding
-    //self.defaultNamespaces = defaultNamespaces
     guard let buf =
       data.withUnsafeBytes({ rbp in rbp.baseAddress?.assumingMemoryBound(to: Int8.self) })
       else { throw Error.invalidSourceData }
     let cfenc = CFStringConvertNSStringEncodingToEncoding(encoding.rawValue)
-    //print(CFStringConvertEncodingToIANACharSetName(cfenc).flatMap { CFStringGetCStringPtr($0, CFStringBuiltInEncodings.UTF8.rawValue) })
     guard
       let iana = CFStringConvertEncodingToIANACharSetName(cfenc) as String?
       else { throw Error.invalidEncoding }
@@ -43,22 +41,22 @@ public final class Doc: Equatable {
     case .html:
       let opt =
         HTML_PARSE_RECOVER.rawValue |
-        HTML_PARSE_NOBLANKS.rawValue |
-        HTML_PARSE_NOWARNING.rawValue |
-        HTML_PARSE_NOERROR.rawValue
+          HTML_PARSE_NOBLANKS.rawValue |
+          HTML_PARSE_NOWARNING.rawValue |
+          HTML_PARSE_NOERROR.rawValue
       _xmlDoc = htmlReadMemory(buf, Int32(data.count), nil, iana, Int32(opt))
     }
     _root = xmlDocGetRootElement(_xmlDoc)
     if _root == nil { throw Error.noXMLRoot }
   }
-
-    convenience init(xmlString: String, kind: Kind) throws{
-        guard let data = xmlString.data(using: .utf8)else{
-            throw Error.invalidSourceData
-        }
-        try self.init(data: data, kind: kind)
+  
+  convenience init(xmlString: String, kind: Kind) throws {
+    guard let data = xmlString.data(using: .utf8) else {
+      throw Error.invalidSourceData
     }
-    
+    try self.init(data: data, kind: kind)
+  }
+  
   convenience init(
     fileURL: URL,
     readingOptions: Data.ReadingOptions = [],
@@ -71,18 +69,18 @@ public final class Doc: Equatable {
       throw err
     }
   }
-
+  
   deinit {
     xmlFreeDoc(_xmlDoc)
   }
-
+  
   public static func == (lhs: Doc, rhs: Doc) -> Bool {
     lhs.data == rhs.data &&
-    lhs.kind == rhs.kind &&
-    lhs.encoding == rhs.encoding &&
-    lhs._xmlDoc == rhs._xmlDoc
+      lhs.kind == rhs.kind &&
+      lhs.encoding == rhs.encoding &&
+      lhs._xmlDoc == rhs._xmlDoc
   }
-
+  
   // MARK: - Privates
   let _xmlDoc: xmlDocPtr!
   let _root: xmlNodePtr!
@@ -95,7 +93,7 @@ public extension Doc {
   var root: Node {
     Node(_doc: self, _xmlNode: _root)
   }
-
+  
   func register(namespaceURI uri: String, forPrefix prefix: String) {
     _namespaces[prefix] = uri
   }
--- Tests/XPaverTests/XPaverTests.swift
+++ Tests/XPaverTests/XPaverTests.swift
@@ -5,14 +5,14 @@ final class XPaverTests: XCTestCase {
   lazy var xmlDoc = try! Doc(fileURL: Self.assetURL(forName: "xml.xml"), kind: .xml)
   lazy var mnsXmlDoc = try! Doc(fileURL: Self.assetURL(forName: "multiple-default-ns.xml"), kind: .xml)
   lazy var htmlDoc = try! Doc(fileURL: Self.assetURL(forName: "html.html"), kind: .html)
-  lazy var xmlStrDoc = try! Doc(xmlString: "<?xml version=\"1.0\" encoding=\"UTF-8\"?><note><to>Tove</to><from>Jani</from><heading>Reminder</heading><body>Don't forget me this weekend!</body></note>", kind: .html)
-
+  lazy var xmlStrDoc = try! Doc(xmlString: "<?xml version=\"1.0\" encoding=\"UTF-8\"?><note><to>Tove</to><from>Jani</from><heading>Reminder</heading><body>Don't forget me this weekend!</body></note>", kind: .xml)
+  
   func testTag() {
     XCTAssertEqual(xmlDoc.root.tag, "package")
     XCTAssertEqual(htmlDoc.root.tag, "html")
-    XCTAssertEqual(htmlDoc.root.tag, "note")
+    XCTAssertEqual(xmlStrDoc.root.tag, "note")
   }
-
+  
   func testSelect() {
     let cNodes = xmlDoc.root.select(xpath: "//dc:contributor")
     XCTAssertEqual(cNodes.count, 6)
@@ -21,77 +21,77 @@ final class XPaverTests: XCTestCase {
     let span = pNodes.first?.first(xpath: "./span")
     XCTAssertNotNil(span)
   }
-
+  
   func testFirst() {
     let hnode = htmlDoc.root.first(xpath: "/html/head/title")
     XCTAssertNotNil(hnode)
     let xnode = xmlDoc.root.first(xpath: "/package/metadata/dc:title")
     XCTAssertNotNil(xnode)
   }
-
+  
   func testTagName() {
     let hnode = htmlDoc.root.first(xpath: "/html/head/title")
     XCTAssertEqual(hnode?.tag, "title")
     let xnode = xmlDoc.root.first(xpath: "/package/metadata/dc:title")
     XCTAssertEqual(xnode?.tag, "title")
   }
-
+  
   func testContent() {
     let cNode = xmlDoc.root.first(xpath: "//dc:contributor")
     XCTAssertEqual(cNode?.content, "O’Reilly Production Services")
     let pNode = htmlDoc.root.first(xpath: "//p")
     XCTAssertEqual(pNode?.content, "Hello, World")
   }
-
+  
   func testRawContent() {
     let cNode = xmlDoc.root.first(xpath: "//dc:contributor")
     XCTAssertEqual(cNode?.rawContent, "<dc:contributor>O’Reilly Production Services</dc:contributor>")
     let pNode = htmlDoc.root.first(xpath: "//p")
     XCTAssertEqual(pNode?.rawContent, #"<p class="foo" id="bar"><span>Hello, World</span></p>"#)
   }
-
+  
   func testInnerRawContent() {
     let cNode = xmlDoc.root.first(xpath: "//dc:contributor")
     XCTAssertEqual(cNode?.innerRawContent, "O’Reilly Production Services")
     let pNode = htmlDoc.root.first(xpath: "//p")
     XCTAssertEqual(pNode?.innerRawContent, "<span>Hello, World</span>")
   }
-
+  
   func testParent() {
     let cNode = xmlDoc.root.first(xpath: "//dc:contributor")
     XCTAssertEqual(cNode?.parent?.tag, "metadata")
   }
-
+  
   func testChildNodes() {
     XCTAssertEqual(xmlDoc.root.childNodes.map(Array.init)?.count, 3)
     XCTAssertEqual(htmlDoc.root.childNodes.map(Array.init)?.count, 2)
     XCTAssertEqual(xmlStrDoc.root.childNodes.map(Array.init)?.count, 4)
   }
-
+  
   func testFirstNode() {
     XCTAssertEqual(xmlDoc.root.firstNode?.tag, "metadata")
     XCTAssertEqual(htmlDoc.root.firstNode?.tag, "head")
     XCTAssertEqual(xmlStrDoc.root.firstNode?.tag, "to")
   }
-
+  
   func testChildNodeAt() {
     XCTAssertEqual(xmlDoc.root.childNode(at: 1)?.tag, "manifest")
     XCTAssertEqual(htmlDoc.root.childNode(at: 1)?.tag, "body")
-      XCTAssertEqual(htmlDoc.root.childNode(at: 1)?.tag, "from")
+    XCTAssertEqual(xmlStrDoc.root.childNode(at: 1)?.tag, "from")
   }
-
+  
   func testPrev() {
     XCTAssertEqual(xmlDoc.root.childNode(at: 1)?.prev, xmlDoc.root.firstNode)
     XCTAssertEqual(htmlDoc.root.childNode(at: 1)?.prev, htmlDoc.root.firstNode)
     XCTAssertEqual(xmlStrDoc.root.childNode(at: 1)?.prev, xmlStrDoc.root.firstNode)
   }
-
+  
   func testNext() {
     XCTAssertEqual(xmlDoc.root.childNode(at: 0)?.next, xmlDoc.root.childNode(at: 1))
     XCTAssertEqual(htmlDoc.root.childNode(at: 0)?.next, htmlDoc.root.childNode(at: 1))
     XCTAssertEqual(xmlStrDoc.root.childNode(at: 0)?.next, xmlStrDoc.root.childNode(at: 1))
   }
-
+  
   func testAttributes() {
     let xattr = xmlDoc.root.first(xpath: "//dc:title")?.attributes?.first { _ in true }
     XCTAssertEqual(xattr?.name, "id")
@@ -103,7 +103,7 @@ final class XPaverTests: XCTestCase {
     XCTAssertEqual(hattr?[1].name, "id")
     XCTAssertEqual(hattr?[1].value, "bar")
   }
-
+  
   func testAttributeValue() {
     let p = htmlDoc.root.first(xpath: "//p")
     XCTAssertEqual(p?.value(forAttribute: "class"), "foo")
@@ -112,7 +112,7 @@ final class XPaverTests: XCTestCase {
     XCTAssertEqual(id?.value(forAttribute: "id"), "pub-identifier")
     XCTAssertEqual(id?.value(forAttribute: "mock:id"), "mock")
   }
-
+  
   func testMultipleDefaultNamespaces() {
     mnsXmlDoc.register(namespaceURI: "http://www.your.example.com/xml/person", forPrefix: "p")
     mnsXmlDoc.register(namespaceURI: "http://www.my.example.com/xml/cities", forPrefix: "c")
@@ -120,7 +120,7 @@ final class XPaverTests: XCTestCase {
     XCTAssertNotNil(cityName)
     XCTAssertEqual(cityName?.content, "London")
   }
-
+  
   func testEval() {
     var result = htmlDoc.root.eval(expr: "count(//p)")
     XCTAssertEqual(result, .double(2))
@@ -129,16 +129,16 @@ final class XPaverTests: XCTestCase {
     result = htmlDoc.root.eval(expr: "boolean(//p[1][.='Hello, World'])")
     XCTAssertEqual(result, .bool(true))
   }
-
+  
   private static func assetURL(forName name: String) -> URL {
     URL(fileURLWithPath: #file)
       .deletingLastPathComponent()
       .appendingPathComponent("assets/\(name)")
   }
-
-// I don't what to do with this, test only work in Xcode right now
   
-//  static var allTests = [
-//    ("testTag", testTag),
-//  ]
+  // I don't what to do with this, test only work in Xcode right now
+  
+  //  static var allTests = [
+  //    ("testTag", testTag),
+  //  ]
 }
